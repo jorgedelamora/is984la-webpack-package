0390995dc3626654ae7c5c92df0fe1e296c731ff
jorgedelamora <jorgedlmv@gmail.com>
2025-09-03 15:56:24 -0600

    initial package structure

---
+++ helpers.js
@@ -0,0 +1,9 @@
+const helperFunc = (string) => {
+	console.log("I helped");
+
+	return string + "World";
+};
+
+module.exports = {
+	helperFunc,
+};
---
+++ index.js
@@ -0,0 +1,7 @@
+const { helperFunc } = require("./helpers");
+
+const string = "Hello ";
+
+module.exports = {
+	string: helperFunc(string),
+};
